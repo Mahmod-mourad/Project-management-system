d756c7c9b78ad5486507d033d45a222d00f1b125
Mahmod-mourad <muradmahmoud682@gmail.com>
2023-09-05 16:33:00 +0300

    add global css and root app layout with Inter font

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { GeistSans } from "geist/font/sans"
+import { GeistMono } from "geist/font/mono"
+import { Analytics } from "@vercel/analytics/next"
+import { Suspense } from "react"
+import { AuthProvider } from "@/components/auth/auth-provider"
+import "./globals.css"
+
+export const metadata: Metadata = {
+  title: "نظام إدارة الأعمال المتكامل | ERP System",
+  description: "نظام إدارة الأعمال المتكامل مع دعم اللغة العربية - Complete ERP System with Arabic Support",
+  generator: "v0.app",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+        <AuthProvider>
+          <Suspense fallback={null}>{children}</Suspense>
+        </AuthProvider>
+        <Analytics />
+      </body>
+    </html>
+  )
+}

b808093c70e65481e399de245b3292177c6f0989
Mahmod-mourad <muradmahmoud682@gmail.com>
2023-09-18 10:00:00 +0300

    add tenant dto - create and update shapes

---
+++ backend/src/tenant/dto/tenant.dto.ts
@@ -0,0 +1,31 @@
+import { IsString, IsOptional, IsEnum, IsObject } from "class-validator"
+import { ApiProperty, PartialType } from "@nestjs/swagger"
+
+export enum TenantStatus {
+  ACTIVE = "active",
+  SUSPENDED = "suspended",
+  CANCELLED = "cancelled",
+}
+
+export class CreateTenantDto {
+  @ApiProperty()
+  @IsString()
+  name: string
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  domain?: string
+
+  @ApiProperty({ enum: TenantStatus, default: TenantStatus.ACTIVE })
+  @IsOptional()
+  @IsEnum(TenantStatus)
+  status?: TenantStatus
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsObject()
+  settings?: Record<string, any>
+}
+
+export class UpdateTenantDto extends PartialType(CreateTenantDto) {}

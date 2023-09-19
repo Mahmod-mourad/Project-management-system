261c2ad8573d0822ac92d86d4ba399caf874acc8
Mahmod-mourad <muradmahmoud682@gmail.com>
2023-09-19 09:55:00 +0300

    add tenant controller - crud and member management

---
+++ backend/src/tenant/tenant.controller.ts
@@ -0,0 +1,49 @@
+import { Controller, Get, Post, Patch, Param, Delete, UseGuards } from "@nestjs/common"
+import { ApiTags, ApiOperation, ApiBearerAuth } from "@nestjs/swagger"
+import type { TenantService } from "./tenant.service"
+import type { CreateTenantDto, UpdateTenantDto } from "./dto/tenant.dto"
+import { TenantGuard } from "../common/guards/tenant.guard"
+
+@ApiTags("tenants")
+@ApiBearerAuth()
+@Controller("tenants")
+export class TenantController {
+  constructor(private readonly tenantService: TenantService) {}
+
+  @Post()
+  @ApiOperation({ summary: "Create a new tenant" })
+  create(createTenantDto: CreateTenantDto) {
+    return this.tenantService.create(createTenantDto)
+  }
+
+  @Get()
+  @ApiOperation({ summary: "Get all tenants" })
+  findAll() {
+    return this.tenantService.findAll()
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get tenant by ID' })
+  findOne(@Param('id') id: string) {
+    return this.tenantService.findOne(id);
+  }
+
+  @Get(':id/stats')
+  @UseGuards(TenantGuard)
+  @ApiOperation({ summary: 'Get tenant statistics' })
+  getStats(@Param('id') id: string) {
+    return this.tenantService.getTenantStats(id);
+  }
+
+  @Patch(":id")
+  @ApiOperation({ summary: "Update tenant" })
+  update(@Param('id') id: string, updateTenantDto: UpdateTenantDto) {
+    return this.tenantService.update(id, updateTenantDto)
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete tenant' })
+  remove(@Param('id') id: string) {
+    return this.tenantService.remove(id);
+  }
+}

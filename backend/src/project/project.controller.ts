4eed715d974124f66706ab2b13a3d324b929c08c
Mahmod-mourad <muradmahmoud682@gmail.com>
2023-09-22 09:10:00 +0300

    add project controller - list, create, update, delete

---
+++ backend/src/project/project.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, Get, Post, Patch, Param, Delete, UseGuards, Headers } from "@nestjs/common"
+import { ApiTags, ApiOperation, ApiBearerAuth, ApiHeader } from "@nestjs/swagger"
+import type { ProjectService } from "./project.service"
+import type { CreateProjectDto, UpdateProjectDto } from "./dto/project.dto"
+import { TenantGuard } from "../common/guards/tenant.guard"
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard"
+
+@ApiTags("projects")
+@ApiBearerAuth()
+@ApiHeader({ name: "x-tenant-id", required: true })
+@UseGuards(JwtAuthGuard, TenantGuard)
+@Controller("projects")
+export class ProjectController {
+  constructor(private readonly projectService: ProjectService) {}
+
+  @Post()
+  @ApiOperation({ summary: "Create a new project" })
+  create(createProjectDto: CreateProjectDto, @Headers('x-tenant-id') tenantId: string) {
+    return this.projectService.create(tenantId, createProjectDto)
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all projects for tenant' })
+  findAll(@Headers('x-tenant-id') tenantId: string) {
+    return this.projectService.findAll(tenantId);
+  }
+
+  @Get(":id")
+  @ApiOperation({ summary: "Get project by ID" })
+  findOne(@Param('id') id: string, @Headers('x-tenant-id') tenantId: string) {
+    return this.projectService.findOne(tenantId, id)
+  }
+
+  @Get(":id/stats")
+  @ApiOperation({ summary: "Get project statistics" })
+  getStats(@Param('id') id: string, @Headers('x-tenant-id') tenantId: string) {
+    return this.projectService.getProjectStats(tenantId, id)
+  }
+
+  @Patch(":id")
+  @ApiOperation({ summary: "Update project" })
+  update(@Param('id') id: string, updateProjectDto: UpdateProjectDto, @Headers('x-tenant-id') tenantId: string) {
+    return this.projectService.update(tenantId, id, updateProjectDto)
+  }
+
+  @Delete(":id")
+  @ApiOperation({ summary: "Delete project" })
+  remove(@Param('id') id: string, @Headers('x-tenant-id') tenantId: string) {
+    return this.projectService.remove(tenantId, id)
+  }
+}
